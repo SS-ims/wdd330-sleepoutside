b1b3027ada4c8d5d45504f017e7b5b3b08249b61
chilltomato <haz23003@byui.edu>
2026-09-10 18:31:09 -0400

    fixed an error related to merging a branch

--- src/js/product.js
+++ src/js/product.js
@@ -6,11 +6,6 @@ const productId = getParam('product');
 
 console.log(dataSource.findProductById(productId));
 
-=======
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
-
-const dataSource = new ProductData("tents");
 function addProductToCart(product) {
   const cartItems = getLocalStorage("so-cart") || [];
   cartItems.push(product);
